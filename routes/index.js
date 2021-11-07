93bfee0321572aeb71acaffb2a1f2169328a57c0
Etherio <50423290+etherio97@users.noreply.github.com>
2021-11-07 15:26:46 +0630

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,9 @@
-const { Router } = require("express");
-const router = Router();
+const router = require("express").Router();
 
 router.get("/", (req, res) => {
   res.send("Hello World");
 });
+
+router.use("/users", require("./users"));
 
 module.exports = router;

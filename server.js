4f10756b03d5e2e5f3c973e95c14ed0b2bf837c7
Etherio <50423290+etherio97@users.noreply.github.com>
2021-11-07 15:21:17 +0630

    Update server.js

--- server.js
+++ server.js
@@ -4,7 +4,6 @@ const router = require("./router");
 const errorHandler = require("./src/error-handler");
 const app = express();
 
-app.use(express.json());
 app.use(errorHandler);
 app.use(router);
 
